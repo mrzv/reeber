7aeb50aaa142c0d09712814450aca39cee162ef6
Dmitriy Morozov <dmitriy@mrzv.org>
2014-11-12 09:48:47 -0800

    Moved out e/local-global/merge-tree-block.h

---
+++ examples/local-global/merge-tree-block.h
@@ -0,0 +1,55 @@
+#ifndef REEBER_MERGE_TREE_BLOCK_H
+#define REEBER_MERGE_TREE_BLOCK_H
+
+#include <diy/serialization.hpp>
+
+#include <reeber/merge-tree.h>
+#include <reeber/grid.h>
+#include <reeber/box.h>
+#include <reeber/merge-tree-serialization.h>
+namespace r = reeber;
+
+
+typedef     REEBER_REAL                     Real;
+
+struct MergeTreeBlock
+{
+    typedef     r::Grid<Real, 3>                Grid;
+    typedef     r::OffsetGrid<Real, 3>          OffsetGrid;
+    typedef     Grid::Index                     Index;
+    typedef     Grid::Vertex                    Vertex;
+    typedef     Grid::Value                     Value;
+    typedef     r::Box<3>                       Box;
+    typedef     r::MergeTree<Index, Value>      MergeTree;
+
+    static void*            create()                                        { return new MergeTreeBlock; }
+    static void             destroy(void* b)                                { delete static_cast<MergeTreeBlock*>(b); }
+    static void             save(const void* b, diy::BinaryBuffer& bb)      { diy::save(bb, *static_cast<const MergeTreeBlock*>(b)); }
+    static void             load(      void* b, diy::BinaryBuffer& bb)      { diy::load(bb, *static_cast<MergeTreeBlock*>(b)); }
+
+    Box                     local;
+    Box                     global;
+    MergeTree               mt;
+};
+
+namespace diy
+{
+    template<>
+    struct Serialization<MergeTreeBlock>
+    {
+        static void             save(diy::BinaryBuffer& bb, const MergeTreeBlock& b)
+        {
+            diy::save(bb, b.local);
+            diy::save(bb, b.global);
+            diy::save(bb, b.mt);
+        }
+        static void             load(diy::BinaryBuffer& bb, MergeTreeBlock& b)
+        {
+            diy::load(bb, b.local);
+            diy::load(bb, b.global);
+            diy::load(bb, b.mt);
+        }
+    };
+}
+
+#endif
--- examples/local-global/mt-np-local-global.cpp
+++ examples/local-global/mt-np-local-global.cpp
@@ -8,62 +8,15 @@
 #include <diy/master.hpp>
 #include <diy/assigner.hpp>
 #include <diy/storage.hpp>
-#include <diy/serialization.hpp>
 #include <diy/decomposition.hpp>
 #include <diy/reduce.hpp>
 #include <diy/partners/swap.hpp>
 #include <diy/io/numpy.hpp>
 #include <diy/io/block.hpp>
 
-#include <reeber/merge-tree.h>
-#include <reeber/grid.h>
-#include <reeber/box.h>
-#include <reeber/merge-tree-serialization.h>
-namespace r = reeber;
-
 #include "format.h"
 
-typedef     REEBER_REAL                     Real;
-
-struct MergeTreeBlock
-{
-    typedef     r::Grid<Real, 3>                Grid;
-    typedef     r::OffsetGrid<Real, 3>          OffsetGrid;
-    typedef     Grid::Index                     Index;
-    typedef     Grid::Vertex                    Vertex;
-    typedef     Grid::Value                     Value;
-    typedef     r::Box<3>                       Box;
-    typedef     r::MergeTree<Index, Value>      MergeTree;
-
-    static void*            create()                                        { return new MergeTreeBlock; }
-    static void             destroy(void* b)                                { delete static_cast<MergeTreeBlock*>(b); }
-    static void             save(const void* b, diy::BinaryBuffer& bb)      { diy::save(bb, *static_cast<const MergeTreeBlock*>(b)); }
-    static void             load(      void* b, diy::BinaryBuffer& bb)      { diy::load(bb, *static_cast<MergeTreeBlock*>(b)); }
-
-    Box                     local;
-    Box                     global;
-    MergeTree               mt;
-};
-
-namespace diy
-{
-    template<>
-    struct Serialization<MergeTreeBlock>
-    {
-        static void             save(diy::BinaryBuffer& bb, const MergeTreeBlock& b)
-        {
-            diy::save(bb, b.local);
-            diy::save(bb, b.global);
-            diy::save(bb, b.mt);
-        }
-        static void             load(diy::BinaryBuffer& bb, MergeTreeBlock& b)
-        {
-            diy::load(bb, b.local);
-            diy::load(bb, b.global);
-            diy::load(bb, b.mt);
-        }
-    };
-}
+#include "merge-tree-block.h"
 
 // Load the specified chunk of data, compute local merge tree, add block to diy::Master
 struct LoadComputeAdd
