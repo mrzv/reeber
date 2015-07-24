ce79fa3b04e03211ef574a38e0cf35416a16a8b7
Gunther H. Weber <GHWeber@lbl.gov>
2015-07-24 16:22:59 -0700

    Change BoxLib IO interface to resemble NumPy IO; combine mt-[np|bl]-local-global.cpp into single source

--- include/reeber/io/boxlib.h
+++ include/reeber/io/boxlib.h
@@ -8,6 +8,9 @@
 #include <algorithm>
 #include <numeric>
 
+// Boost
+#include <boost/algorithm/string/split.hpp>
+
 // DIY
 #include <diy/types.hpp>
 #include <diy/mpi.hpp>
@@ -47,15 +50,36 @@ namespace BoxLib
                   typedef       std::vector<int>                                         Shape;
                   typedef       std::vector<int>                                         Vertex;
                   typedef       std::vector<std::string>                                 VarNameList;
+        private:
+                  struct ReadInfo {
+                                   ReadInfo(std::string name):
+                                       filename(""), varname(""), finest_fill_level(-1)
+                                   {
+                                       if (name.empty()) throw std::runtime_error("No filename specified");
+                                       std::vector<std::string> parts;
+                                       parts = boost::split(parts, name, std::bind1st(std::equal_to<char>(), ':'));
+                                       filename = parts[0];
+                                       if (parts.size() > 1) varname = parts[1];
+                                       if (parts.size() > 2) varname = parts[2];
+                                   }
+
+                      std::string  filename;
+                      std::string  varname;
+                      int          finest_fill_level;
+                  };
         public:
                   // Construct reader, get domain and variable names in file.
                   reader(std::string            filename,
                          diy::mpi::communicator communicator):
                       communicator_(communicator),
-                      dataServices_(filename.c_str(), Amrvis::NEWPLT)
+                      read_info(filename),
+                      dataServices_(read_info.filename.c_str(), Amrvis::NEWPLT)
                   {
                       if(!dataServices_.AmrDataOk())
+                      {
+                          LOG_SEV(fatal) << "Opening " << read_info.filename << " failed";
                           throw std::runtime_error("Cannot open BoxLib file");
+                      }
 
                       // Get domain extents for all levels
                       const int finest_level = dataServices_.AmrDataRef().FinestLevel();
@@ -77,26 +101,46 @@ namespace BoxLib
                       const Array<string>& plotVarNames = dataServices_.AmrDataRef().PlotVarNames();
                       for (int i = 0; i < plotVarNames.size(); ++i)
                           varnames_.push_back(plotVarNames[i]);
+
+                      // If varname is not set, use first variable defined in file
+                      if (read_info.varname.empty())
+                      {
+                          read_info.varname = varnames_[0];
+                      }
+                      // Otherwise check, if specified variable exists
+                      else if (std::find(varnames_.begin(), varnames_.end(), read_info.varname) == varnames_.end())
+                      {
+                          LOG_SEV_IF(communicator.rank() == 0, fatal) << "Variable " << read_info.varname << " does not exist in file";
+                          throw std::runtime_error("Specified variable does not exit in file");
+                      }
+
+                      // If level is not set, default to finest level in file
+                      if (read_info.finest_fill_level < 0)
+                          read_info.finest_fill_level = finest_level;
+                      // Otherwise check, if specified level exists
+                      else if (read_info.finest_fill_level > finest_level)
+                      {
+                          LOG_SEV_IF(communicator.rank() == 0, warning) << "Level " << read_info.finest_fill_level << " does not exist, using finest level in file (" << finest_level << ") instead";
+                          read_info.finest_fill_level = finest_level;
+                      }
+                      LOG_SEV(debug) << "BoxLib reader constructed: varname=" << read_info.varname << " finest_fill_level = " << read_info.finest_fill_level;
                   }
 
-                  const int          finestLevel()         const                          { return dataServices_.AmrDataRef().FinestLevel(); }
+                  const int          finest_level()        const                          { return dataServices_.AmrDataRef().FinestLevel(); }
                   const Shape&       shape(int level = 0)  const                          { return shape_[level]; }
                   const Vertex&      from(int level = 0)   const                          { return from_[level]; }
                   const Vertex&      to(int level = 0)     const                          { return to_[level]; }
                   const VarNameList& varnames()            const                          { return varnames_; }
 
+                  void               set_read_variable(std::string varname)               { read_info.varname = varname; }    // FIXME: Add check if variable exists
+                  void               set_read_level(int level)                            { read_info.finest_fill_level = std::max(std::min(level, dataServices_.AmrDataRef().FinestLevel()), 0); }
+
                   void               read(const diy::DiscreteBounds& bounds,    // Region to read
                                           Real* buffer,                         // Buffer where data will be copied to
-                                          std::string varname = std::string(),  // Name of variable to use; if empty string, read first variable in file
-                                          int finestFillLevel = 0) const        // Finest level to reead/level to flatten to
+                                          bool collective = true) const         // Collective I/O? (FIXME: Non-collective I/O not implemented, yet)
                   {
-                      // If varname is not set, use first variable defined in file
-                      if (varname.empty())
-                      {
-                          // No varname speciied, use first in file
-                          varname = varnames_[0];
-                          LOG_SEV(info) << "No variable name specified. Using " << varname << " (first variable in file)";
-                      }
+                      // FIXME: Hack. Either implement non-collective I/O or remove flag
+                      if (!collective) throw std::runtime_error("Non-collective reading not implemented for BoxLib reader");
 
                       // BoxLib expects a list of boxes to read on all ranks.
                       // Gather the bounds for the read requests on all processors.
@@ -124,7 +168,7 @@ namespace BoxLib
                       MultiFab mf;
                       BoxArray mf_boxes(partition_boxes);
                       mf.define(mf_boxes, 1, 0, dm, Fab_allocate);
-                      dataServices_.AmrDataRef().FillVar(mf, finestFillLevel, varname);
+                      dataServices_.AmrDataRef().FillVar(mf, read_info.finest_fill_level, read_info.varname);
 
                       // Copy the data from BoxLib to Reeber2
                       const FArrayBox& my_fab = mf[ParallelDescriptor::MyProc()];
@@ -141,6 +185,7 @@ namespace BoxLib
 
         private:
                   diy::mpi::communicator     communicator_;
+                  ReadInfo                   read_info;     // What to read. Struct before dataServices_ because it parses the filename to be passed to dataServices constructor.
                   mutable DataServices       dataServices_; // Hack, declare "mutable" since BoxLib does not declare const funrctions for reading
                   std::vector < Shape >      shape_;
                   std::vector < Vertex >     from_;
