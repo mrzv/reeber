0ef68894f8ae24eb0726997c2e76a1c40403e44b
Arnur Nigmetov <nigmetov@tugraz.at>
2020-08-06 09:53:33 -0700

    Fix dgms/integral output in amr-connected-components with diy::SharedOutFile
    SharedOutFile was created per block, not per rank for diagrams. Fixed.
    
    Each MPI rank printed its portion of persistent integral
    to a separate file. Switched to SharedOutFile.

--- examples/include/output-persistence.h
+++ examples/include/output-persistence.h
@@ -15,24 +15,24 @@ struct OutputPairs
 
     struct ExtraInfo
     {
-        ExtraInfo(const std::string& outfn_, bool verbose_, diy::mpi::communicator& _world):
-            outfn(outfn_), verbose(verbose_), world(_world)   {}
+        ExtraInfo(const std::string& _outfn, bool _verbose, diy::mpi::communicator& _world):
+            outfn(_outfn), verbose(_verbose), world(_world),  ofs(_outfn, _world) {}
 
         std::string         outfn;
         bool                verbose;
         diy::mpi::communicator& world;
+        diy::io::SharedOutFile ofs;
     };
 
     using Neighbor = typename Block::Neighbor;
 
-    OutputPairs(const Block& _block, const ExtraInfo& _extra, const LocalFunctor& _test_local, RealType _threshold, bool _ignore_zero_persistence):
+    OutputPairs(const Block& _block, ExtraInfo* _extra, const LocalFunctor& _test_local, RealType _threshold, bool _ignore_zero_persistence):
         negate(_block.get_merge_tree().negate()),
         ignore_zero_persistence(_ignore_zero_persistence),
         threshold(_threshold),
         block(_block),
         extra(_extra),
-        test_local(_test_local),
-        ofs(extra.outfn, extra.world)
+        test_local(_test_local)
     {
     }
 
@@ -41,14 +41,14 @@ struct OutputPairs
         if (!test_local(block, from))
             return;
 
-        if (extra.verbose)
+        if (extra->verbose)
         {
             std::string s;
             if (from != to)
                 s = fmt::format("{} {} {} {} {} {}\n", from->vertex, from->value, through->vertex, through->value, to->vertex, to->value);
             else
                 s = fmt::format("{} {} {} --\n",       from->vertex,  from->value, (block.get_merge_tree().negate() ? "-inf" : "inf"));
-            ofs << s;
+            extra->ofs << s;
         } else
         {
             RealType birth_time, death_time;
@@ -76,7 +76,7 @@ struct OutputPairs
 
 //            fmt::print("PERSISTENCE {} {}\n", birth_time, death_time);
 
-            ofs <<  birth_time << " " <<  death_time << "\n";
+            extra->ofs <<  birth_time << " " <<  death_time << "\n";
         }
     }
 
@@ -84,15 +84,13 @@ struct OutputPairs
     const bool             ignore_zero_persistence;
     const RealType         threshold;
     const Block&           block;
-    const ExtraInfo&       extra;
+    ExtraInfo*             extra;
     const LocalFunctor&    test_local;
-    mutable diy::io::SharedOutFile ofs;
-
 };
 
 template<class Block, class LocalFunctor>
 void output_persistence(Block* b, const diy::Master::ProxyWithLink& cp,
-                        const typename OutputPairs<Block, LocalFunctor>::ExtraInfo& extra,
+                        typename OutputPairs<Block, LocalFunctor>::ExtraInfo* extra,
                         const LocalFunctor& test_local,
                         typename Block::RealType threshold,
                         bool  _ignore_zero_persistence)
